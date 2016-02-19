21721fd2f046959846a110653f863dcf388aa536
freek <freek@spatie.be>
2016-02-19 17:02:55 +0000

    improve null coalesce test

--- tests/stubs/it-can-replace-null-coalesce-operators/php5.php
+++ tests/stubs/it-can-replace-null-coalesce-operators/php5.php
@@ -2,9 +2,10 @@
 
 class Test
 {
-    function myFunction($input = null)
+    function myFunction($input, $input2, $input3)
     {
         $result = isset($input) ? $input : 'fixed-value';
+        $result = isset($input) ? $input : (isset($input2) ? $input2 : $input3);
         return $result;
     }
 }
--- tests/stubs/it-can-replace-null-coalesce-operators/php7.php
+++ tests/stubs/it-can-replace-null-coalesce-operators/php7.php
@@ -2,9 +2,11 @@
 
 class Test
 {
-    function myFunction($input = null) : string
+    function myFunction($input, $input2, $input3)
     {
         $result = $input ?? 'fixed-value';
+
+        $result = $input ?? $input2 ?? $input3;
 
         return $result;
     }
