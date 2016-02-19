40f03193c342c947f1538e1c157e9d5a2824d728
freek <freek@spatie.be>
2016-02-19 15:29:02 +0000

    add return types test

---
+++ tests/stubs/it-can-remove-return-types/php5.php
@@ -0,0 +1,12 @@
+<?php
+class Test
+{
+    function myFunction()
+    {
+        return 5;
+    }
+    function anotherFunction()
+    {
+        return 5;
+    }
+}
---
+++ tests/stubs/it-can-remove-return-types/php7.php
@@ -0,0 +1,14 @@
+<?php
+
+class Test
+{
+    function myFunction() : string
+    {
+        return 5;
+    }
+
+    function anotherFunction() : MyClass
+    {
+        return 5;
+    }
+}
