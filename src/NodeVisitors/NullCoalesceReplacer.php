6209b76170c1c5d9d30229ea2beed7771e2a7490
Sergey Demchuk <sergiusd@megaplan.ru>
2017-09-25 11:38:58 +0300

    Null collapse for functions and methods

--- src/NodeVisitors/NullCoalesceReplacer.php
+++ src/NodeVisitors/NullCoalesceReplacer.php
@@ -16,9 +16,20 @@ class NullCoalesceReplacer extends NodeVisitorAbstract
         if (!$node instanceof Coalesce) {
             return;
         }
-
-        $issetCall = new Node\Expr\FuncCall(new Node\Name('isset'), [$node->left]);
-
-        return new Node\Expr\Ternary($issetCall, $node->left, $node->right);
+        switch(true)
+        {
+            case $node->left instanceof Node\Expr\FuncCall:
+            case $node->left instanceof Node\Expr\MethodCall:
+            case $node->left instanceof Node\Expr\StaticCall:
+                $notEmptyCall = new Node\Expr\BooleanNot(new Node\Expr\FuncCall(new Node\Name('empty'), [$node->left]));
+                return new Node\Expr\Ternary($notEmptyCall, $node->left, $node->right);
+            case $node->left instanceof Node\Expr\BinaryOp:
+                $issetCall = new Node\Expr\FuncCall(new Node\Name('isset'), [$node->left->right]);
+                $node->left->right = new Node\Expr\Ternary($issetCall, $node->left->right, $node->right);
+                return $node->left;
+            default:
+                $issetCall = new Node\Expr\FuncCall(new Node\Name('isset'), [$node->left]);
+                return new Node\Expr\Ternary($issetCall, $node->left, $node->right);
+        }
     }
 }
